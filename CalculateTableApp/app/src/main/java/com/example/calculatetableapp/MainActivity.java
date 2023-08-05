391c0460b0d598ece9c0e59c58d0e83ff4da174b
Nitesharu1998 <ide9892nitesh@gmail.com>
2023-08-05 11:01:43 +0530

    added code for data binding and comments for implementation

--- CalculateTableApp/app/src/main/java/com/example/calculatetableapp/MainActivity.java
+++ CalculateTableApp/app/src/main/java/com/example/calculatetableapp/MainActivity.java
@@ -1,48 +1,42 @@
 package com.example.calculatetableapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.calculatetableapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
-
-    EditText getTableValue;
-    View submit;
-    TextView showTable;
-
+    /*TODO Step 3: Create object of dataBinding: XML name and binding in the end*/
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /*TODO Step 4: initiate the layout*/
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        getTableValue = findViewById(R.id.getTableValue);
-        submit = findViewById(R.id.submit);
-        showTable = findViewById(R.id.showTable);
-
-        submit.setOnClickListener(new View.OnClickListener() {
+       binding.submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                Toast.makeText(MainActivity.this , "Button CLicked" , Toast.LENGTH_LONG).show();
 
-                if(getTableValue.length() <= 0){
-                    showTable.setText("");
+                if(binding.getTableValue.length() <= 0){
+                    binding.showTable.setText("");
 
                     Toast.makeText(MainActivity.this , "Invalid Number !" , Toast.LENGTH_LONG).show();
                 }
                 else{
-                    int number = Integer.parseInt(getTableValue.getText().toString());
+                    int number = Integer.parseInt(binding.getTableValue.getText().toString());
 //                    Toast.makeText((Context) MainActivity.this, "Data is Here" , Toast.LENGTH_LONG).show();
-                    showTable.setText("");
+                    binding.showTable.setText("");
                     for(int i = 1 ; i<= 10 ; i++){
-                        showTable.append(number+" * "+i +" = "+number*i+"\n");
+                        binding.showTable.append(number+" * "+i +" = "+number*i+"\n");
                     }
-                    getTableValue.setText("");
+                    binding.getTableValue.setText("");
                 }
 
 
@@ -53,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clear(View v){
-        getTableValue.setText("");
-        showTable.setText("");
+        binding.getTableValue.setText("");
+        binding. showTable.setText("");
 
     }
 }
